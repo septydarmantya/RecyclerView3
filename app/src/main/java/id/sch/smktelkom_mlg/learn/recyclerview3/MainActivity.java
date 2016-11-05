f3371f4ec2b30bdbf463f8696450466140280555
septy <septydarmantya@gmail.com>
2016-11-06 00:19:40 +0700

    RecyclerView Search

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -7,9 +7,11 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -29,6 +31,10 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
     int itemPost;
+    ArrayList<Hotel> mListAll = new ArrayList<>();
+    boolean isFiltered;
+    ArrayList<Integer> mListMapFilter = new ArrayList<>();
+    String mQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,8 +94,50 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mQuery = newText.toLowerCase();
+                doFilter(mQuery);
+                return true;
+            }
+
+            private void doFilter(String mQuery) {
+                if (!isFiltered) {
+                    mListAll.clear();
+                    mListAll.addAll(mList);
+                    isFiltered = true;
+                }
+
+                mList.clear();
+                if (mQuery == null || mQuery.isEmpty()) {
+                    mList.addAll(mListAll);
+                    isFiltered = false;
+                } else {
+                    mListMapFilter.clear();
+                    for (int i = 0; i < mListAll.size(); i++) {
+                        Hotel hotel = mListAll.get(i);
+                        if (hotel.judul.toLowerCase().contains(mQuery) ||
+                                hotel.deskripsi.toLowerCase().contains(mQuery) ||
+                                hotel.lokasi.toLowerCase().contains(mQuery)) {
+                            mList.add(hotel);
+                            mListMapFilter.add(i);
+                        }
+                    }
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
         return true;
     }
 
